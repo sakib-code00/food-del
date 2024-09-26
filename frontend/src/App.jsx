58507852743a1b5a0323a12a82257ef0937a68af
Sakibul Islam <asakibul.bd@gmail.com>
2024-09-26 23:02:19 +0600

    navbar is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './components/Navbar'
+import Navbar from './components/Navbar'
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-     
-    </>
+    <div className='app'>
+      <Navbar></Navbar>
+      
+    </div>
   )
 }
 
